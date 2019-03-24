cb4861014387c0899f29fdcf3ac1360afeefd49b
Afik Shani Schonwetter <afik.ss@Afiks-MacBook-Pro.local>
2019-03-24 10:52:02 +0200

    new project structure

---
+++ client/app/src/main/java/com/example/afikshani/milab_final/MapsActivity.java
@@ -0,0 +1,214 @@
+package com.example.afikshani.milab_final;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.ResultReceiver;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private String currentPolyline;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String destination = bundle.getString("destination");
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in location and move the camera
+        LatLng sourceLocation = new LatLng(32.1761179, 34.8362246);
+        LatLng destinationLocation = new LatLng(32.0752857, 34.775322);
+
+        mMap.addMarker(new MarkerOptions().position(sourceLocation).title("IDC Herzliya"));
+        mMap.addMarker(new MarkerOptions().position(destinationLocation).title("Dizengoff Center"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sourceLocation, 16.0f));
+
+        String routeRequest = getDirectionsUrl("IDC Herzliya", "Dizengoff Center");
+
+        RouteCalc routeCalc = new RouteCalc();
+
+        routeCalc.execute(routeRequest);
+    }
+
+    private String getDirectionsUrl(String origin, String destination) {
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=AIzaSyA55Fgqx8yShAamvF7B3llMO3ZrIKBZyAs" + "&mode=bicycling+" + "&alternatives=" + true;
+
+        return url;
+    }
+
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
+    }
+
+
+
+    private class RouteCalc extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            String routeData = "";
+
+            try {
+                routeData = downloadUrl(url[0]);
+
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return routeData;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            ParserTask parserTask = new ParserTask();
+
+            parserTask.execute(result);
+
+        }
+
+    }
+
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
+
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList();
+                lineOptions = new PolylineOptions();
+
+                List<HashMap<String, String>> path = result.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+                    builder.include(position);
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(12);
+                lineOptions.color(Color.BLUE);
+                lineOptions.geodesic(true);
+
+            }
+
+            // Drawing polyline in the Google Map for the i-th route
+            mMap.addPolyline(lineOptions);
+            LatLngBounds bounds = builder.build();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 50));
+        }
+    }
+
+}
---
+++ client/app/src/main/java/com/example/afikshani/milab_final/RouteCalc.java
@@ -0,0 +1,109 @@
+package com.example.afikshani.milab_final;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class RouteCalc extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_VOLLEY = "com.example.afikshani.milab_final.action.ACTION_VOLLEY";
+
+    // TODO: Rename parameters
+    private static final String PARAM1_ORIGIN = "com.example.afikshani.milab_final.extra.PARAM1";
+    private static final String PARAM2_DEST = "com.example.afikshani.milab_final.extra.PARAM2";
+    private static final String RESPONSE_PARAM = "com.example.afikshani.milab_final.extra.RESPONSE";
+    public static final String RESPONSE_ANSWER = "com.example.afikshani.milab_final.extra.ANSWER";
+
+
+
+    public RouteCalc() {
+        super("RouteCalc");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void initService(Context context, ResultReceiver receiver, String destination) {
+        Intent intent = new Intent(context, RouteCalc.class);
+        intent.setAction(ACTION_VOLLEY);
+
+        intent.putExtra(PARAM1_ORIGIN,"IDC Herzliya");
+        intent.putExtra(PARAM2_DEST, "Dizengoff Center Mall");
+        intent.putExtra(RESPONSE_PARAM, receiver);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_VOLLEY.equals(action)) {
+                final String origin = intent.getStringExtra(PARAM1_ORIGIN);
+                final String dest = intent.getStringExtra(PARAM2_DEST);
+                final ResultReceiver receiver = intent.getParcelableExtra(RESPONSE_PARAM);
+                handleActionProvideRoute(origin, dest, receiver);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionProvideRoute(String origin, String destination, final ResultReceiver receiver) {
+        final RequestQueue queue = Volley.newRequestQueue(this);
+        final String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+origin+"&destination="+destination+"&key=AIzaSyA55Fgqx8yShAamvF7B3llMO3ZrIKBZyAs"+"&alternatives="+true;
+
+        StringRequest req = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("hello", "Response back from server - " + response);
+                final Bundle bundle = new Bundle();
+                bundle.putString(RESPONSE_ANSWER, response);
+                receiver.send(200, bundle);
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("routes", "Error - " + error);
+            }
+        });
+
+        queue.add(req);
+    }
+
+
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionBaz(String param1, String param2) {
+        // TODO: Handle action Baz
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
